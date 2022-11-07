48dde79e5aa9bbc681cb7c59745e6738349deab5
jon.he <jon.he@imaginato.com>
2022-11-07 11:11:32 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 import getConfig from 'next/config';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const IndexPage: NextPage = () => {
-  const { publicRuntimeConfig } = getConfig();
-  console.log(publicRuntimeConfig?.buildTime);
+  useEffect(() => {
+    const { publicRuntimeConfig } = getConfig();
+    console.log(publicRuntimeConfig?.buildTime);
+  });
   return (<div />);
 };
 
